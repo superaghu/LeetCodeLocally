e094c3edb3700935c34e46b96b5295fa8cc012bf
rmamadgi <rmamadgi@expedia.com>
2020-04-26 23:54:31 -0700

    copied implementation

---
+++ src/main/java/interview/exercises/LC10RegularExpressionMatching.java
@@ -0,0 +1,7 @@
+package interview.exercises;
+
+public class LC10RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
---
+++ src/test/java/interview/LC10RegularExpressionMatchingTest.java
@@ -0,0 +1,25 @@
+package interview;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import interview.exercises.LC10RegularExpressionMatching;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LC10RegularExpressionMatchingTest {
+    private LC10RegularExpressionMatching program;
+
+    @BeforeMethod
+    public void setUp() {
+        program = new LC10RegularExpressionMatching();
+    }
+
+    @Test
+    public void testName() {
+        assertThat(program.isMatch("aa", "a")).isEqualTo(false);
+        assertThat(program.isMatch("aa", "a*")).isEqualTo(true);
+        assertThat(program.isMatch("ab", ".*")).isEqualTo(true);
+        assertThat(program.isMatch("aab", "c*a*b")).isEqualTo(true);
+        assertThat(program.isMatch("mississippi", "mis*is*p*.")).isEqualTo(false);
+    }
+}
