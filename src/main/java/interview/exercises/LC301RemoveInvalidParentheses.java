4c7e62b23b8a809e178e5e4d168d388f0c5a8686
rmamadgi <rmamadgi@expedia.com>
2020-05-02 23:08:25 -0700

    test cases

---
+++ src/main/java/interview/exercises/LC301RemoveInvalidParentheses.java
@@ -0,0 +1,12 @@
+package interview.exercises;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/remove-invalid-parentheses/">Link</a>
+ */
+public class LC301RemoveInvalidParentheses {
+    public List<String> removeInvalidParentheses(String s) {
+        return List.of();
+    }
+}
---
+++ src/test/java/interview/LC301RemoveInvalidParenthesesTest.java
@@ -0,0 +1,23 @@
+package interview;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import interview.exercises.LC301RemoveInvalidParentheses;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LC301RemoveInvalidParenthesesTest {
+    private LC301RemoveInvalidParentheses program;
+
+    @BeforeMethod
+    public void setUp() {
+        program = new LC301RemoveInvalidParentheses();
+    }
+
+    @Test
+    public void testName() {
+        assertThat(program.removeInvalidParentheses("()())()")).containsExactly("()()()", "(())()");
+        assertThat(program.removeInvalidParentheses("(a)())()")).containsExactly("(a)()()", "(a())()");
+        assertThat(program.removeInvalidParentheses(")(")).containsExactly("");
+    }
+}
