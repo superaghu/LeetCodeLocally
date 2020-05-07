82640026cc49bf9c6c92a533606671646459ca53
pvovveti <pavanvovveti@gmail.com>
2020-05-06 22:37:44 -0700

    First commit by pavan

---
+++ src/main/java/interview/exercises/LC238ProductofArrayExceptSelf.java
@@ -0,0 +1,11 @@
+package interview.exercises;
+
+/**
+ * @see <a href="https://leetcode.com/problems/product-of-array-except-self/">Link</a>
+ */
+public class LC238ProductofArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+}
---
+++ src/test/java/interview/LC238ProductofArrayExceptSelfTest.java
@@ -0,0 +1,16 @@
+package interview;
+
+import interview.exercises.LC238ProductofArrayExceptSelf;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC238ProductofArrayExceptSelfTest {
+
+    @Test
+    public void testName() {
+        LC238ProductofArrayExceptSelf program = new LC238ProductofArrayExceptSelf();
+        assertThat(program.productExceptSelf(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{24, 12, 8, 6});
+        assertThat(program.productExceptSelf(new int[]{4, 5, 1, 2})).isEqualTo(new int[]{10, 8, 40, 20});
+    }
+}
