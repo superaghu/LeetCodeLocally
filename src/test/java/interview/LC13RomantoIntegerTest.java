8e9146a130a314ef9ce5a5639e6c88c91105209d
rmamadgi <rmamadgi@expedia.com>
2020-05-10 00:49:43 -0700

    fixed scala compiler error

--- src/test/java/interview/LC13RomantoIntegerTest.java
+++ src/test/java/interview/LC13RomantoIntegerTest.java
@@ -2,7 +2,7 @@ package interview;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import interview.java.exercises.LC13RomantoInteger;
+import interview.scala.exercises.LC13RomantoInteger;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
