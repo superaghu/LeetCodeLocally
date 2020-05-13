c77dee8644a9a217ea84a9f46fb0c7e0462acaf4
rmamadgi <rmamadgi@expedia.com>
2020-05-13 00:17:54 -0700

    Revert: remove raghu items

---
+++ src/main/java/interview/java/rmsolutions/LC138CopyListwithRandomPointer.java
@@ -0,0 +1,33 @@
+package interview.java.rmsolutions;
+
+import interview.java.model.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @see <a href="https://leetcode.com/problems/copy-list-with-random-pointer/">See LC</a>
+ */
+public class LC138CopyListwithRandomPointer {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> visited = new HashMap<>();
+        Node headPointer = head;
+        Node copyHead = new Node(0, null, null);
+        Node copyHeadPointer = copyHead;
+        while (head != null) {
+            final Node copy = new Node(head.val, null, null);
+            copyHead.next = copy;
+            visited.put(head, copy);
+            copyHead = copyHead.next;
+            head = head.next;
+        }
+        while (headPointer != null) {
+            Node randomToBeAdded = visited.get(headPointer);
+            if (headPointer.random != null) {
+                randomToBeAdded.random = visited.get(headPointer.random);
+            }
+            headPointer = headPointer.next;
+        }
+        return copyHeadPointer.next;
+    }
+}
