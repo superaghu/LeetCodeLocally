a5661850dd5a6c335cace5213d50cdf22c8d9f16
rmamadgi <rmamadgi@expedia.com>
2020-05-17 17:17:03 -0700

    add additional tests

--- src/test/java/interview/LC91DecodeWaysTest.java
+++ src/test/java/interview/LC91DecodeWaysTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import interview.java.LC91DecodeWays;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-@Test(enabled = false)
+@Ignore
+@Test
 public class LC91DecodeWaysTest {
     private LC91DecodeWays program;
 
@@ -18,5 +20,11 @@ public class LC91DecodeWaysTest {
     public void testGenerate() {
         assertThat(program.numDecodings("12")).isEqualTo(2);
         assertThat(program.numDecodings("226")).isEqualTo(3);
+        assertThat(program.numDecodings("0")).isEqualTo(0);
+        assertThat(program.numDecodings("1")).isEqualTo(1);
+        assertThat(program.numDecodings("10")).isEqualTo(1);
+        assertThat(program.numDecodings("01")).isEqualTo(0);
+        assertThat(program.numDecodings("100")).isEqualTo(0);
+        assertThat(program.numDecodings("110")).isEqualTo(1);
     }
 }
